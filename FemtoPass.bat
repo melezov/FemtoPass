449d9b23fcfc28ecb5d0409860b49077c2245782
Marko Elezović <marko@element.hr>
2013-09-04 09:22:36 +0200

    Added output conversion to UTF-8

--- FemtoPass.bat
+++ FemtoPass.bat
@@ -1,26 +1,38 @@
 @echo off
 setlocal
 
-set GoTools=%~dp0GoTools\
+set Source=%~dp0
+set GoTools=%Source%GoTools\
 
 set FP_STEPS=4
 set FP_PSE=
 set FP_ARCH=/x32
-set FP_UNICODE=
+set FP_UNICODE=/d UNICODE=1
 
 :loopy
+rem if "%1" == "/x32" goto set_x32
 rem if "%1" == "/x64" goto set_x64
+rem if "%1" == "/ansi" goto set_ansi
 rem if "%1" == "/unicode" goto set_unicode
 if "%1" == "/run" goto set_run
 if "%1" == "/pause" goto set_pause
 if "%1" == "/?" goto show_help
+
 :shifty
 shift
 if "%1" == "" goto entry
 goto loopy
 
+:set_x32
+set FP_ARCH=/x32
+goto shifty
+
 :set_x64
 set FP_ARCH=/x64
+goto shifty
+
+:set_ansi
+set FP_UNICODE=
 goto shifty
 
 :set_unicode
@@ -43,8 +55,8 @@ echo.
 echo Switches are as follows:
 rem echo (*) /x32     - builds a 32-bit (x86) variant of the program (default)
 rem echo     /x64     - builds a 64 bit (AMD64) variant of the program
-rem echo (*) /ansi    - builds a ANSI variant of the program (default)
-rem echo     /unicode - builds a UNICODE version of the program
+rem echo (*) /unicode - builds a UNICODE version of the program (default)
+rem echo     /ansi    - builds a ANSI variant of the program
 echo     /run     - executes the program after building it
 echo     /pause   - pauses after the build/run steps
 echo.
@@ -57,11 +69,11 @@ echo.
 echo Building FemtoPass (/? for help):
 echo.
 
-if exist "Binary" goto out_exists
-mkdir Binary
+if exist "%Source%Binary" goto out_exists
+mkdir "%Source%Binary"
 
 :out_exists
-cd Binary
+cd "%Source%Binary"
 echo Step #(1/%FP_STEPS%) CLEANUP
 if exist "FemtoPass.bin" del "FemtoPass.bin"
 if exist "FemtoPass.bin" goto errcln
@@ -72,24 +84,24 @@ if exist "FemtoPass.obj" goto errcln
 if exist "FemtoPass.exe" del "FemtoPass.exe"
 if exist "FemtoPass.exe" goto errcln
 
-cd ..\Source
+cd "%Source%Source"
 echo Step #(2/%FP_STEPS%) RESOURCES
 "%GoTools%GoRC.exe" /b /ni /r /fo "..\Binary\FemtoPass.res" "FemtoPass.rc"
 if errorlevel 1 goto errres
 
-cd ..\Source
+cd "%Source%Source"
 echo Step #(3/%FP_STEPS%) ASSEMBLY
 "%GoTools%GoAsm.exe" /b /ni %FP_ARCH% %FP_UNICODE% /fo "..\Binary\FemtoPass.obj" "FemtoPass.asm"
 if errorlevel 1 goto errasm
 
-cd ..\Binary
+cd "%Source%Binary"
 echo Step #(4/%FP_STEPS%) LINKER
 "%GoTools%GoLink.exe" /console /ni /fo "FemtoPass.exe" /entry FemtoPass_Entry "FemtoPass.obj" "FemtoPass.res" kernel32.dll
 if errorlevel 1 goto errlink
 
 if "%FP_STEPS%" == "4" goto Bye
 
-cd ..\Binary
+cd "%Source%Binary"
 echo Step #(5/%FP_STEPS%) EXECUTION
 start FemtoPass
 goto Bye
--- Source/FemtoPass.asm
+++ Source/FemtoPass.asm
@@ -8,13 +8,16 @@
 
 ; =============================================================================
 
-        #define MAX_PASSWORD 1024
+        #define MAX_PASSWORD         128
+        #define MAX_PASSWORD_UTF     (MAX_PASSWORD * 3)
 
         szConin        dss    "CONIN$", 0h
         szPassword     dss    MAX_PASSWORD dup ?
+        szPasswordUtf8 db     MAX_PASSWORD_UTF dup ?
         ddConinHandle  dd     ?
         ddConinMode    dd     ?
         ddCharsRead    dd     ?
+        ddBytesInUtf8  dd     ?
         ddBytesWritten dd     ?
 
 ; #############################################################################
@@ -29,51 +32,59 @@
             inc esi ; 1
             invoke CreateFile, ADDR szConin, GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL
             cmp eax, INVALID_HANDLE_VALUE
-            je >>.exit
+            je >
             mov [ ddConinHandle ], eax
 
             inc esi ; 2
             invoke GetConsoleMode, [ ddConinHandle ], ADDR ddConinMode
             or eax, eax
-            je >>.exit
+            je >
             mov eax, [ ddConinMode ]
-            
+
             inc esi ; 3
             and eax, ~ENABLE_ECHO_INPUT
             invoke SetConsoleMode, [ ddConinHandle ], eax
             or eax, eax
-            je >.exit
+            je >
 
             inc esi ; 4
             invoke ReadConsole, [ ddConinHandle ], ADDR szPassword, MAX_PASSWORD, ADDR ddCharsRead, NULL
             or eax, eax
-            je >.exit
+            je >
 
             inc esi ; 5
             invoke SetConsoleMode, [ ddConinHandle ], [ ddConinMode ]
             or eax, eax
-            je >.exit
+          : je >
 
             inc esi ; 6
-            invoke GetStdHandle, STD_OUTPUT_HANDLE
-            cmp eax, INVALID_HANDLE_VALUE
-            je >.exit
+            mov ebx, [ ddCharsRead ]
+            dec ebx
+            dec ebx
+            invoke WideCharToMultiByte, CP_UTF8, 0, ADDR szPassword, ebx, ADDR szPasswordUtf8, MAX_PASSWORD_UTF, NULL, NULL
+            or eax, eax
+            je >
+            mov [ ddBytesInUtf8 ], eax
 
             inc esi ; 7
-            invoke WriteFile, eax, ADDR szPassword, [ ddCharsRead ], ADDR ddBytesWritten, NULL
-            or eax, eax
-            je >.exit
+            invoke GetStdHandle, STD_OUTPUT_HANDLE
+            cmp eax, INVALID_HANDLE_VALUE
+            je >
 
             inc esi ; 8
-            mov eax, [ ddCharsRead ]
+            invoke WriteFile, eax, ADDR szPasswordUtf8, [ ddBytesInUtf8 ], ADDR ddBytesWritten, NULL
+            or eax, eax
+            je >
+
+            inc esi ; 9
+            mov eax, [ ddBytesInUtf8 ]
             sub eax, [ ddBytesWritten ]
-            jne >.exit
+            jne >
             ;{
                 ret
             ;}
 
-        .exit:
-            mov eax, esi
+          : mov eax, esi
             ret
         ;}
 
